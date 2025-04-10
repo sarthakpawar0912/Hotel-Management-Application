0a8e350b6a72e63aa6ab62a686fa781490de3bd2
sarthak pawar <pawarsr06@gmail.com>
2025-04-10 23:23:41 +0530

    Fixed bugs and updated code

--- src/main/java/com/sarthakpawar/SERVICES/CUSTOMER/bookings/BookingService.java
+++ src/main/java/com/sarthakpawar/SERVICES/CUSTOMER/bookings/BookingService.java
@@ -6,5 +6,7 @@ import com.sarthakpawar.DTO.ReservationResponseDto;
 public interface BookingService {
 
     boolean postReservation(ReservationDto reservationDto);
+
     ReservationResponseDto getAllReservationByUserId(Long userId, int pageNumber);
+
 }
