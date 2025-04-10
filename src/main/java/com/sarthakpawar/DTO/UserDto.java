d0401c9c52156772ae777ba9527bcbc0207a52a9
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-10 23:16:04 +0530

    Fixed bugs and updated code

--- src/main/java/com/sarthakpawar/DTO/UserDto.java
+++ src/main/java/com/sarthakpawar/DTO/UserDto.java
@@ -7,59 +7,11 @@ import lombok.Data;
 public class UserDto {
 
     private Long id;
+
     private String email;
+
     private String name;
+
     private UserRole userRole;
 
-    public UserDto(Long id, String email, String name, UserRole userRole) {
-        this.id = id;
-        this.email = email;
-        this.name = name;
-        this.userRole = userRole;
-    }
-
-    public UserDto() {
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public UserRole getUserRole() {
-        return userRole;
-    }
-
-    public void setUserRole(UserRole userRole) {
-        this.userRole = userRole;
-    }
-
-    @Override
-    public String toString() {
-        return "UserDto{" +
-                "id=" + id +
-                ", email='" + email + '\'' +
-                ", name='" + name + '\'' +
-                ", userRole=" + userRole +
-                '}';
-    }
 }
