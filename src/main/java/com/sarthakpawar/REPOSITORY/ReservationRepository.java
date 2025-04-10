5ee79d30b5d37b83604e519411a5cfd3770ec95d
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-10 23:17:31 +0530

    Fixed bugs and updated code

--- src/main/java/com/sarthakpawar/REPOSITORY/ReservationRepository.java
+++ src/main/java/com/sarthakpawar/REPOSITORY/ReservationRepository.java
@@ -8,5 +8,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation,Long> {
+
     Page<Reservation> findAllByUserId(Pageable pageable, Long userId);
+
 }
