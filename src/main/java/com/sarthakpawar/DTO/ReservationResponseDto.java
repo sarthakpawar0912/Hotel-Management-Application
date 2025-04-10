9812543eeeb948e3db0411ccc2e2451858a82ebe
sarthakpawar0912 <pawarsr06@gmail.com>
2025-04-10 23:14:47 +0530

    Fixed bugs and updated code

--- src/main/java/com/sarthakpawar/DTO/ReservationResponseDto.java
+++ src/main/java/com/sarthakpawar/DTO/ReservationResponseDto.java
@@ -7,49 +7,10 @@ import java.util.List;
 @Data
 public class ReservationResponseDto {
 
-
     private Integer totalPages;
+
     private Integer pageNumber;
+
     private List<ReservationDto> reservationDtoList;
-    public ReservationResponseDto() {
-    }
 
-    @Override
-    public String toString() {
-        return "ReservationResponseDto{" +
-                "totalPages=" + totalPages +
-                ", pageNumber=" + pageNumber +
-                ", reservationDtoList=" + reservationDtoList +
-                '}';
-    }
-
-    public Integer getTotalPages() {
-        return totalPages;
-    }
-
-    public void setTotalPages(Integer totalPages) {
-        this.totalPages = totalPages;
-    }
-
-    public Integer getPageNumber() {
-        return pageNumber;
-    }
-
-    public void setPageNumber(Integer pageNumber) {
-        this.pageNumber = pageNumber;
-    }
-
-    public List<ReservationDto> getReservationDtoList() {
-        return reservationDtoList;
-    }
-
-    public void setReservationDtoList(List<ReservationDto> reservationDtoList) {
-        this.reservationDtoList = reservationDtoList;
-    }
-
-    public ReservationResponseDto(Integer totalPages, Integer pageNumber, List<ReservationDto> reservationDtoList) {
-        this.totalPages = totalPages;
-        this.pageNumber = pageNumber;
-        this.reservationDtoList = reservationDtoList;
-    }
 }
