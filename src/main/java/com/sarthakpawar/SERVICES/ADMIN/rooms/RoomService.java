b562b2d491a28fcc98ebd4390f91434c18059097
sarthak pawar <pawarsr06@gmail.com>
2025-04-10 23:21:11 +0530

    Fixed bugs and updated code

--- src/main/java/com/sarthakpawar/SERVICES/ADMIN/rooms/RoomService.java
+++ src/main/java/com/sarthakpawar/SERVICES/ADMIN/rooms/RoomService.java
@@ -4,9 +4,15 @@ import com.sarthakpawar.DTO.RoomDto;
 import com.sarthakpawar.DTO.RoomsResponseDto;
 
 public interface RoomService {
+
     boolean postRoom(RoomDto roomDto);
+
     RoomsResponseDto getAllRooms(int pageNumber);
+
     RoomDto getRoomById(Long id);
+
     boolean updateRoom(Long id,RoomDto roomDto);
+
     void deleteRoom(Long id);
+
 }
