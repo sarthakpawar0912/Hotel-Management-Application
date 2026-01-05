c89a6215724709a755c51962bf9503b1a0eb7e8d
sarthak pawar <pawarsr06@gmail.com>
2026-01-05 18:26:48 +0530

    Fixed bugs and updated code

--- src/main/java/com/sarthakpawar/SERVICES/AUTH/AuthServiceImpl.java
+++ src/main/java/com/sarthakpawar/SERVICES/AUTH/AuthServiceImpl.java
@@ -9,7 +9,6 @@ import jakarta.annotation.PostConstruct;
 import jakarta.persistence.EntityExistsException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
